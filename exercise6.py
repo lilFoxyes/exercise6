ecd0bd6d98484d7dbda7377a5af080cfc45fd014
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 16:35:59 -0300

    exercise6.py

--- exercise6.py
+++ exercise6.py
@@ -1,2 +1,2 @@
 n = int(input("Escreva um número e te darei o dobro, o triplo e a raiz quadrada dele dele:"))
-print("O seu número é: {}, o dobro é: {}, o triplo é: {} e a raiz quadrada é: {}".format(n, n*2, n*3, n**0.5))
+print("O seu número é: {}, o dobro é: {}, o triplo é: {} e a raiz quadrada é: {:.2f}".format(n, n*2, n*3, n**0.5))
